319b68dfa7c87784565b22bb63e81df1f92430f3
Gihun Ham <progh2@gmail.com>
2019-07-31 13:24:03 +0900

    Init Project

---
+++ app/src/main/java/kr/hs/emirim/progh2/punchpower/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.emirim.progh2.punchpower
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
